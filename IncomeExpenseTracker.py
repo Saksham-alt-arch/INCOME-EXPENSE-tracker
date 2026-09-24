652dc4ef0efa7809b4cf7c4f86f0e7658a05ecf2
Saksham-alt-arch <ktmsaksham@gmail.com>
2026-09-24 09:55:44 -0500

    Working on the top part

--- IncomeExpenseTracker.py
+++ IncomeExpenseTracker.py
@@ -1,9 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
 
 class widget(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Income-Expense-Tracker")
+        self.setWindowIcon(QIcon("INCOME-EXPENSE-tracker\\BankImage.jpg"))
+        self.setGeometry(600,150,800,700)
+        self.initUI()
+
+    def initUI(self):
+        # Top label
+        self.top_label = QLabel("INCOME-EXPENSE-TRACKER",self)
+        self.top_label.setGeometry(0,0,799,50)
+
+        # Top Image to go with the top label
+        self.image = QPixmap("INCOME-EXPENSE-tracker\\BankImage.jpg")
+        self.bank_pic = QLabel(self)
+        self.bank_pic.setPixmap(self.image)
+        self.bank_pic.setGeometry(0,0,75,50)
+        self.bank_pic.setScaledContents(True)
+
+        # Setting CSS styles
+        self.setStyleSheet("""
+                background-color: hsl(275, 8%, 28%);
+            top_label{
+                    font_size: 40px;
+            }
+        """
+        )
+        print("Hello")
 
 def main():
     app = QApplication(sys.argv)
